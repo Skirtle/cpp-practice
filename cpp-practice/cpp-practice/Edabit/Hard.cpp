80ddf6451dd9095c95e300b32685ffe6eb6fc2cf
Dalton <daltonkajander@knights.ucf.edu>
2022-05-21 02:44:18 -0400

    a

--- cpp-practice/cpp-practice/Edabit/Hard.cpp
+++ cpp-practice/cpp-practice/Edabit/Hard.cpp
@@ -7,10 +7,6 @@ int roots(int a, int b, int c);
 double discount(int base, int discount);
 std::vector<int> evenOddTransform(std::vector<int> arr, int n);
 
-// Main
-int main() {
-}
-
 int roots(int a, int b, int c) { return (-b + (sqrt(b*b - 4*a*c))) / (2 * a); }
 double discount(int base, int discount) { return (base * (100 - discount) * 0.01l); }
 std::vector<int> evenOddTransform(std::vector<int> arr, int n) {
